8a37ce1b7c1efd50a1a16e7ac3965c545122231c
johnstef99 <johnstef1999@gmail.com>
2020-12-19 17:08:37 +0200

    fix bug that generated one less benchmark

--- assignment3/ProcessorDescriptionFiles/generate.sh
+++ assignment3/ProcessorDescriptionFiles/generate.sh
@@ -6,13 +6,15 @@ inorder='../../../my_mcpat/mcpat/ProcessorDescriptionFiles/inorder_arm.xml'
 
 config=''
 stats=''
+
 for file in $(find $gem5_outputs -name stats.txt -or -name config.json | sort -h)
 do
-  name=$(echo $stats | rev | cut -d'/' -f2 | rev)
+  name=$(echo $file | rev | cut -d'/' -f2 | rev)
   if [[ $file == *"config.json" ]]; then
     config=$file
   fi
   if [[ $file == *"stats.txt" ]]; then
+    echo $name
     stats=$file
     python2 $gem5_to_mcpat $stats $config $inorder -o "$name.xml"
   fi
--- assignment3/mcpat_output/mcpat_generate.sh
+++ assignment3/mcpat_output/mcpat_generate.sh
@@ -6,6 +6,8 @@ xmls='../ProcessorDescriptionFiles/*.xml'
 for file in $(ls $xmls)
 do
   name=$(echo $file | rev | cut -d'/' -f1 | rev | cut -d'.' -f1)
+  if [[ $name = "specsjeng_2GHz_64_64kB_4_64kB_2_2MB_8" ]]; then
   echo "Generating for $name"
   $mcpat_path -infile $file -print_level 5 > "$name.txt"
+  fi
 done
